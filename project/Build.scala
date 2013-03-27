3ee8a1dc6042fbd3f1f8e9940a0110cb0996da17
porter <brian@porter.net>
2013-03-27 22:40:56 +0100

    inital Commit

---
+++ project/Build.scala
@@ -0,0 +1,31 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "playstartjava"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "org.webjars" % "webjars-play" % "2.1.0",
+    "org.webjars" % "bootstrap" % "2.1.1",
+    "org.webjars" % "html5shiv" % "3.6.1",
+    "org.webjars" % "bootswatch" % "2.2.2+1",
+    "org.webjars" % "bootstrap-datepicker" % "1.0.1",
+    "org.webjars" % "bootstrap-timepicker" % "0.2.1",
+
+    "securesocial" %% "securesocial" % "master-SNAPSHOT",
+    "com.typesafe" %% "play-plugins-mailer" % "2.1-RC2",
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here    
+    resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)  
+  )
+
+}
